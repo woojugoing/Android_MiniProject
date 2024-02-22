30e47f31d790f7c402700e5e87d40d80f13d62f5
Kyungwon Seo <woojugoing@naver.com>
2024-02-22 14:41:57 +0900

    :memo: [feat] Add Webview & Add Internet Permission

--- WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
+++ WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.woojugoing.webtoonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.woojugoing.webtoonapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
+        activityMainBinding.run {
+            webView.run {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true   // 앱 내 js 사용 허용
+                loadUrl("https://google.com")
+            }
+        }
     }
 }
