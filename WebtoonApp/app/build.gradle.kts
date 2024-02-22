9d58b71c1fb0cdf69b9cda01b99a1e90793770d1
Kyungwon Seo <woojugoing@naver.com>
2024-02-22 14:19:11 +0900

    :memo: [feat] Add ktlint in Project

--- WebtoonApp/app/build.gradle.kts
+++ WebtoonApp/app/build.gradle.kts
@@ -33,9 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable = true
+    }
 }
 
+val ktlint by configurations.creating
+val detekt by configurations.creating
+
 dependencies {
+    ktlint("com.pinterest:ktlint:0.49.1") { attributes { attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL)) } }
+    detekt("io.gitlab.arturbosch.detekt:detekt-cli:1.23.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +53,32 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+val ktlintCheck by tasks.registering(JavaExec::class) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args("**/src/**/*.kt", "**.kts", "!**/build/**")
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style and format"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+    args("-F", "**/src/**/*.kt", "**.kts", "!**/build/**")
+}
+
+val detektTask = tasks.register<JavaExec>("detekt") {
+    mainClass.set("io.gitlab.arturbosch.detekt.cli.Main")
+    classpath = detekt
+    args(listOf("-i", projectDir, "-c", "$projectDir/detekt.yml", "-ex", ".*/build/.*,.*/resources/.*"))
+}
+
+tasks.check {
+    dependsOn(ktlintCheck)
+    dependsOn(detektTask)
 }
--- WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
+++ WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.woojugoing.webtoonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojugoing.webtoonapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
