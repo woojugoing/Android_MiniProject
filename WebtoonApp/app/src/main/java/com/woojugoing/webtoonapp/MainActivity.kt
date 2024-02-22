435d82e253e0ee7624fd8ce0ca20693b29c543bb
Kyungwon Seo <woojugoing@naver.com>
2024-02-22 15:05:24 +0900

    :memo: [feat] Fragemnt to MainActivity

--- WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
+++ WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/MainActivity.kt
@@ -2,12 +2,11 @@ package com.woojugoing.webtoonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebViewClient
 import com.woojugoing.webtoonapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         activityMainBinding.run {
-            webView.run {
-                webViewClient = WebViewClient()
-                settings.javaScriptEnabled = true   // 앱 내 js 사용 허용
-                loadUrl("https://google.com")
+            btn1.setOnClickListener {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragmentContainer, WebViewFragment())
+                    commit()
+                }
             }
         }
     }
--- WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/WebViewFragment.kt
+++ WebtoonApp/app/src/main/java/com/woojugoing/webtoonapp/WebViewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.woojugoing.webtoonapp.databinding.FragmentWebviewBinding
 
@@ -14,5 +15,14 @@ class WebViewFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         fragmentWebViewBinding = FragmentWebviewBinding.inflate(inflater)
         return fragmentWebViewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentWebViewBinding.webview.run {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("https://google.com")
+        }
     }
 }
